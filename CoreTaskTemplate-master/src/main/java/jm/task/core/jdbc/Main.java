059344039912a7c57163caeeae2bce0df218db99
bazzukka <Exe9595@gmail.com>
2021-06-26 18:32:37 +0300

    ver_1.2

--- CoreTaskTemplate-master/src/main/java/jm/task/core/jdbc/Main.java
+++ CoreTaskTemplate-master/src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,19 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Ivan", "Ivanov", (byte) 30);
+        userService.saveUser("Oleg", "Olegovoch", (byte) 27);
+        userService.saveUser("Som", "Somov", (byte) 18);
+        userService.saveUser("Tom", "Tomsky", (byte) 5);
+
+        userService.getAllUsers();
+        userService.dropUsersTable();
     }
 }
--- CoreTaskTemplate-master/src/main/java/jm/task/core/jdbc/util/Util.java
+++ CoreTaskTemplate-master/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,28 @@
 package jm.task.core.jdbc.util;
 
+import com.mysql.cj.jdbc.Driver;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private static final String url = "jdbc:mysql://localhost:3306/mysql";
+    private static final String user = "root";
+    private static final String pass = "root";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(url, user, pass);
+//            System.out.println("Connection established");
+        } catch (SQLException e) {
+            System.out.println("driver hasn't been found or connection failed");
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
